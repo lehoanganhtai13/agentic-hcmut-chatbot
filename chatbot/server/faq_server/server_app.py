e90a39b548e48879549587a57ce3aacc18791b14
lehoanganhtai <lehoanganhtai13@gmail.com>
2025-05-14 17:41:06 +0700

    feat: refactor document and FAQ retrieval server initialization to streamline model loading and enhance server startup process

--- chatbot/server/faq_server/server_app.py
+++ chatbot/server/faq_server/server_app.py
@@ -27,81 +27,58 @@ class FAQRetrievalOutput(BaseModel):
 
 # ------------------- Init Server API -------------------
 
-async def load():
-    """Load the FAQ retrieval model."""
-    global retriever 
-    try:
-        # Load the FAQ retrieval model
-        logger.info("Starting up FAQ Retrieval server...")
-        models_config = {}
-        with open("./chatbot/config/models_config.json", "r") as f:
-            # Load the JSON file
-            models_config = json.load(f)
-
-            # Convert the loaded JSON to a ModelsConfig object
-            models_config = ModelsConfig.from_dict(models_config)
-
-        # Initialize the embedder
-        embedder = init_embedder(models_conf=models_config)
-
-        # Initialize the MinIO client for loading BM25 state dicts
-        minio_client = Minio(
-            endpoint=SETTINGS.MINIO_URL.replace("http://", "").replace("https://", ""),
-            access_key=SETTINGS.MINIO_ACCESS_KEY_ID,
-            secret_key=SETTINGS.MINIO_SECRET_ACCESS_KEY,
-            secure=False
-        )
-        bm25_client = BM25Client(
-            storage=minio_client,
-            bucket_name=SETTINGS.MINIO_BUCKET_FAQ_INDEX_NAME,
-            init_without_load=False,
-            remove_after_load=True
-        )
-
-        # Initialize the vector database client
-        vector_db = VectorDatabase(
-            host=SETTINGS.MILVUS_URL.replace("http://", "").split(":")[0],
-            port=int(SETTINGS.MILVUS_URL.split(":")[-1]),
-            run_async=False
-        )
-
-        # Initialize the FAQ retriever
-        retriever = FAQRetriever(
-            collection_name=SETTINGS.MILVUS_COLLECTION_FAQ_NAME,
-            embedder=embedder,
-            bm25_client=bm25_client,
-            vector_db=vector_db
-        )
-        logger.info("FAQ retriever initialized successfully.")
-        return {"status": "success", "message": "FAQ retrieval model loaded successfully."}
-    except Exception as e:
-        logger.error(f"Error loading model: {e}")
-        logger.error(traceback.format_exc())
-        return {"status": "error", "message": str(e)}
-
-@asynccontextmanager
-async def lifespan(mcp: FastMCP):
-    global retriever
-    retriever = None
-
-    try:
-        await load() # Initialize the retriever
-    except Exception as e:
-        logger.error(f"Error during startup: {e}")
-        logger.error(traceback.format_exc())
-        return
-    
-    yield
-    logger.info("Shutting down FAQ Retrieval server...")
-    del retriever
-
-
 mcp = FastMCP(
     name="FAQ Retrieval Server",
-    lifespan=lifespan,
     instructions="This is a server for retrieving relevant FAQs about HCMUT.",
     tags=["faq"]
 )
+
+try:
+    # Load the FAQ retrieval model
+    logger.info("Starting up FAQ Retrieval server...")
+    models_config = {}
+    with open("./chatbot/config/models_config.json", "r") as f:
+        # Load the JSON file
+        models_config = json.load(f)
+
+        # Convert the loaded JSON to a ModelsConfig object
+        models_config = ModelsConfig.from_dict(models_config)
+
+    # Initialize the embedder
+    embedder = init_embedder(models_conf=models_config)
+
+    # Initialize the MinIO client for loading BM25 state dicts
+    minio_client = Minio(
+        endpoint=SETTINGS.MINIO_URL.replace("http://", "").replace("https://", ""),
+        access_key=SETTINGS.MINIO_ACCESS_KEY_ID,
+        secret_key=SETTINGS.MINIO_SECRET_ACCESS_KEY,
+        secure=False
+    )
+    bm25_client = BM25Client(
+        storage=minio_client,
+        bucket_name=SETTINGS.MINIO_BUCKET_FAQ_INDEX_NAME,
+        init_without_load=False,
+        remove_after_load=True
+    )
+
+    # Initialize the vector database client
+    vector_db = VectorDatabase(
+        host=SETTINGS.MILVUS_URL.replace("http://", "").split(":")[0],
+        port=int(SETTINGS.MILVUS_URL.split(":")[-1]),
+        run_async=False
+    )
+
+    # Initialize the FAQ retriever
+    retriever = FAQRetriever(
+        collection_name=SETTINGS.MILVUS_COLLECTION_FAQ_NAME,
+        embedder=embedder,
+        bm25_client=bm25_client,
+        vector_db=vector_db
+    )
+    logger.info("FAQ retriever initialized successfully.")
+except Exception as e:
+    logger.error(f"Error loading model: {e}")
+    logger.error(traceback.format_exc())
 
 # ------------------- API Endpoints -------------------
 
@@ -149,4 +126,11 @@ if __name__ == "__main__":
     asyncio.run(check_mcp(mcp))
 
     # Start the FastMCP server
-    mcp.run(transport="sse", host="0.0.0.0", port=8000)
+    asyncio.run(
+        mcp.run_async(
+            transport="sse",
+            host="0.0.0.0",
+            port=8000,
+            uvicorn_config={"workers": 4}
+        )
+    )
